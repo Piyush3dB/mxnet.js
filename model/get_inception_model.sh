720b41b667738de4c60d8d40a55ee3be597b45eb
Piyush Singh <mrsinghpiyush@gmail.com>
2017-02-28 15:24:03 +0000

    Print top 10 results

--- model/get_inception_model.sh
+++ model/get_inception_model.sh
@@ -17,12 +17,12 @@ wget --no-check-certificate http://data.dmlc.ml/mxnet/models/imagenet/inception-
 tar -zxvf inception-bn.tar.gz
 
 # Create Symbol + params file for JSON
-cp Inception-BN-symbol.json ./Inception-BN-symbol-auto.json
-sed -i '1s/^/{\n"symbol":\n/' ./Inception-BN-symbol-auto.json
-sed -i '$s/$/,/' ./Inception-BN-symbol-auto.json
-echo -en "\n" >> ./Inception-BN-symbol-auto.json
-cat synset.txt | sed 's/.*/"&",/' | tr '\n' ' ' | sed 's/.*/"synset": [&],/' | sed 's/, ],/],/g' >> ./Inception-BN-symbol-auto.json
-echo -en "\n" >> ./Inception-BN-symbol-auto.json
-base64 -w 0 Inception-BN-0126.params | sed 's/.*/"parambase64": "&"/' >> ./Inception-BN-symbol-auto.json
-echo -en "\n" >> ./Inception-BN-symbol-auto.json
-echo } >> ./Inception-BN-symbol-auto.json
+cp Inception-BN-symbol.json ./Inception-BN-symbol-js.json
+sed -i '1s/^/{\n"symbol":\n/' ./Inception-BN-symbol-js.json
+sed -i '$s/$/,/' ./Inception-BN-symbol-js.json
+echo -en "\n" >> ./Inception-BN-symbol-js.json
+cat synset.txt | sed 's/.*/"&",/' | tr '\n' ' ' | sed 's/.*/"synset": [&],/' | sed 's/, ],/],/g' >> ./Inception-BN-symbol-js.json
+echo -en "\n" >> ./Inception-BN-symbol-js.json
+base64 -w 0 Inception-BN-0126.params | sed 's/.*/"parambase64": "&"/' >> ./Inception-BN-symbol-js.json
+echo -en "\n" >> ./Inception-BN-symbol-js.json
+echo } >> ./Inception-BN-symbol-js.json
--- test_on_node.js
+++ test_on_node.js
@@ -1,7 +1,7 @@
 var mx = require("./mxnet_predict.js");
 var model = require("./model/fastpoor.json");
 var model = require("./model/inception-bn-model.json");
-var model = require("./model/data/Inception-BN-symbol-try.json");
+var model = require("./model/data/Inception-BN-symbol-js.json");
 var cat_encoded = require("./data/cat.base64.json");
 var decode = mx.base64Decode(cat_encoded);
 var decoded = new Float32Array(decode.buffer);
@@ -16,11 +16,25 @@ for (var step = 0; nleft != 0; ++step) {
   console.log("progress " + (step+1) + "/" + (nleft+step+1));
 }
 out = pred.output(0);
-max_index = 0;
-for (var i = 0; i < out.data.length; ++i) {
-  if (out.data[max_index] < out.data[i]) max_index = i;
+
+console.log("finished prediction....");
+out = pred.output(0);
+var index = new Array();
+for (var i=0;i<out.data.length;i++) {
+	index[i] = i;
 }
-console.log('Top-1: ' + model.synset[max_index] + ', value=' + out.data[max_index]);
+max_output = 10;
+console.log("Max output = " + max_output);
+index.sort(function(a,b) {return out.data[b]-out.data[a];});
+//var end = new Date().getTime();
+//var time = (end - start) / 1000;
+//console.log("time-cost=" + time + " sec");
+for (var i = 0; i < max_output; i++) {
+	//console.log('Top-' + (i+1) + ':' + model.synset[index[i]] + ', value=' + out.data[index[i]]);
+	console.log("Top-%d: %s, value=%f", (i+1), model.synset[index[i]], out.data[index[i]]);
+}
+
+
 pred.destroy();
 
 //   docker run -it --rm --name my-running-script -v "$PWD":/usr/src/app -w /usr/src/app node:4 node test_on_node.js
