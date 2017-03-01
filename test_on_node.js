b5c5294b0d286b082005a71cc86cc1c6676d4bda
Piyush Singh <mrsinghpiyush@gmail.com>
2017-03-01 14:51:15 +0000

    Add timer

--- test_on_node.js
+++ test_on_node.js
@@ -11,9 +11,15 @@ pred = new mx.Predictor(model, {'data': [1, 3, 224, 224]});
 pred.setinput('data', cat);
 console.log("Here");
 var nleft = 1;
+
+var start = new Date().getTime();
+var end = new Date().getTime();
+var time = (end - start) / 1000;
 for (var step = 0; nleft != 0; ++step) {
   nleft = pred.partialforward(step);
-  console.log("progress " + (step+1) + "/" + (nleft+step+1));
+  end = new Date().getTime();
+  time = (end - start) / 1000;
+  console.log("progress " + (step+1) + "/" + (nleft+step+1) + "  Time=" + time + "s");
 }
 out = pred.output(0);
 
@@ -38,3 +44,5 @@ for (var i = 0; i < max_output; i++) {
 pred.destroy();
 
 //   docker run -it --rm --name my-running-script -v "$PWD":/usr/src/app -w /usr/src/app node:4 node test_on_node.js
+//   docker run -v "$PWD":/usr/src/app -w /usr/src/app node:4 node test_on_node.js
+//   docker run -v ${PWD}:/src apiaryio/base-emscripten-dev emcc"
