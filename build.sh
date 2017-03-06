14f9f9327e396f00e4d4052b63a4fd9dee2ebe4d
Piyush Singh <mrsinghpiyush@gmail.com>
2017-03-06 20:04:55 +0000

    Improve readme documentation

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ MXNET=mxnet
 echo "Rebuild libmxnet_predict.js from MXNet with emscripten"
 rm -rf libmxnet_predict.js*
 cd ${MXNET}
+git pull
 git submodule update --init --recursive
 cd amalgamation/
 make clean libmxnet_predict.js MIN=1 EMCC="docker run -v ${PWD}:/src apiaryio/emcc emcc"
