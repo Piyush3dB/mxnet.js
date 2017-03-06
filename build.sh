8cfe54c5e000ec8cccb34d137de6f440c20672ee
Piyush Singh <mrsinghpiyush@gmail.com>
2017-03-06 15:28:06 +0000

    Some mods to build script

--- build.sh
+++ build.sh
@@ -2,16 +2,12 @@
 # MXNet checkout directory
 MXNET=mxnet
 
-# Emscripten executable
-#EMCC="docker run -v ${PWD}:/src apiaryio/emcc emcc"
-
 # Build
 echo "Rebuild libmxnet_predict.js from MXNet with emscripten"
+rm -rf libmxnet_predict.js*
 cd ${MXNET}
-git checkout 33930c6b89314739f4f39c3fd642db5e024b8dc2
-git submodule update --init --recursive
+#git submodule update --init --recursive
 cd ${MXNET}/amalgamation/
 make clean libmxnet_predict.js MIN=1 EMCC="docker run -v ${PWD}:/src apiaryio/emcc emcc"
-#make clean libmxnet_predict.js MIN=1 EMCC=$EMCC
 cd -
 cp ${MXNET}/amalgamation/libmxnet_predict.js* .
